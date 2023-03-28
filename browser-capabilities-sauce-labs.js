b214e0cf06f53848dc18b852f38966d20a2a7644
Ionut Horgos <horgosdumitru@gmail.com>
2023-03-28 21:45:01 +0300

    refactoring

---
+++ browser-capabilities-sauce-labs.js
@@ -0,0 +1,46 @@
+module.exports = {
+  chrome: {
+    browserName: 'chrome',
+    browserVersion: 'latest',
+    platformName: 'macOS 12',
+    'goog:chromeOptions': {
+      args: [
+        '--enable-automation',
+        '--use-fake-ui-for-media-stream',
+        '--use-fake-device-for-media-stream',
+        '--use-file-for-fake-video-capture=/Users/chef/Downloads/btc_qr_address_video.mjpeg',
+      ],
+    },
+    acceptInsecureCerts: true,
+    'sauce:options': {
+      screenResolution: '2048x1536',
+    },
+  },
+
+  firefox: {
+    browserName: 'firefox',
+    browserVersion: 'latest',
+    platformName: 'macOS 12',
+    'sauce:options': {
+      screenResolution: '2048x1536',
+    },
+  },
+
+  safari: {
+    browserName: 'safari',
+    browserVersion: '15',
+    platformName: 'macOS 12',
+    'sauce:options': {
+      screenResolution: '2048x1536',
+    },
+  },
+
+  edge: {
+    browserName: 'MicrosoftEdge',
+    browserVersion: 'latest',
+    platformName: 'Windows 10',
+    'sauce:options': {
+      screenResolution: '1920x1080',
+    },
+  },
+};
--- wdio.conf.remote.js
+++ wdio.conf.remote.js
@@ -2,6 +2,8 @@
 //   chrome: { version: '110.0.5481.77' }, // https://chromedriver.chromium.org/
 //   firefox: { version: '0.30.0' }, // https://github.com/mozilla/geckodriver/releases
 // };
+const browsersCapabilities = require('./browser-capabilities-sauce-labs');
+
 exports.config = {
   //
   // ====================
@@ -62,27 +64,27 @@ exports.config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://saucelabs.com/platform/platform-configurator
   //
-  // capabilities: [browsersCapabilities[firefox]],
-  capabilities: [{
+ capabilities: [browsersCapabilities[chrome]],
+//   capabilities: [{
     
-    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    // grid with only 5 firefox instances available you can make sure that not more than
-    // 5 instances get started at a time.
-    maxInstances: 5,
-    //
-    browserName: 'chrome',
-    acceptInsecureCerts: true,
-    // If outputDir is provided WebdriverIO can capture driver session logs
-    // it is possible to configure which logTypes to include/exclude.
-    // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-    // excludeDriverLogs: ['bugreport', 'server'],
-    // 'goog:chromeOptions':{
-    //     args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage']
-    // }
-    // 'goog:chromeOptions':{
-    //     args: ['--disable-gpu', '--disable-dev-shm-usage']
-    // }
-}],
+//     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+//     // grid with only 5 firefox instances available you can make sure that not more than
+//     // 5 instances get started at a time.
+//     maxInstances: 5,
+//     //
+//     browserName: 'chrome',
+//     acceptInsecureCerts: true,
+//     // If outputDir is provided WebdriverIO can capture driver session logs
+//     // it is possible to configure which logTypes to include/exclude.
+//     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+//     // excludeDriverLogs: ['bugreport', 'server'],
+//     // 'goog:chromeOptions':{
+//     //     args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage']
+//     // }
+//     // 'goog:chromeOptions':{
+//     //     args: ['--disable-gpu', '--disable-dev-shm-usage']
+//     // }
+// }],
   //
   // ===================
   // Test Configurations
