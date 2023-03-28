7c6b68c4d9836b133abdcfb68666d924a34de97d
Ionut Horgos <horgosdumitru@gmail.com>
2023-03-28 14:39:04 +0300

    add chromeOptions

--- wdio.conf.js
+++ wdio.conf.js
@@ -68,11 +68,15 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
+        'goog:chromeOptions':{
+            args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage']
+        }
+
     }],
     //
     // ===================
