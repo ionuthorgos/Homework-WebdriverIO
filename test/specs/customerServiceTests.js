3d725934b336d15a9ef98e977990f5f86b241e5b
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-21 14:43:45 +0300

    valid test

--- test/specs/customerServiceTests.js
+++ test/specs/customerServiceTests.js
@@ -19,40 +19,40 @@ describe('Validate Customer Service URL', () => {
     it('Validate that the default travellers input is 2 travellers 1 room', async () => {
         await GenericFunctions.validateElementContainsText(GeneralSelectors.travellers_button, "text", ['Travellers:', '2 travellers, 1 room'])
     })
-    it('Select randomly from the list a location from Going to', async () => {
-        await GenericFunctions.clickElement(GeneralSelectors.going_to_button)
-        await GeneralPage.selectRandomLocationGoingTo()
-        await GenericFunctions.clickElement(GeneralSelectors.first_element_going_to_list)
-    })
-    it('Select random dates from DatePicker and Apply button', async () => {
-        await GenericFunctions.clickElement(GeneralSelectors.dates_button)
-        await GeneralPage.selectRandomDatesFromDatePicker()
-        await GenericFunctions.clickElement(GeneralSelectors.apply_button)
-    })
-    it('Select random Travellers', async () => {
-        await GenericFunctions.clickElement(GeneralSelectors.travellers_button)
-        await GeneralPage.addRandomTravellers()
-    })
-    it('Click the done button', async () => {
-        await GenericFunctions.clickElement(GeneralSelectors.done_button)
-        await GenericFunctions.validateAttributeElement(GeneralSelectors.travellers_container, "aria-hidden", "true")
-    })
-    it('Click the search button ', async () => {
-        await GenericFunctions.waitForElementToBeExpectedState(GeneralSelectors.search_button, 'be clickable')
-        await GenericFunctions.clickElement(GeneralSelectors.search_button)
-    })
-    it('Click What we are paid impacts our sort order link', async () => {
-        await GenericFunctions.waitForElementToBeExpectedState(GeneralSelectors.impacts_sort_order_link, 'be clickable')
-        await GenericFunctions.clickElement(GeneralSelectors.impacts_sort_order_link)
-        await browser.pause(3000)
-        await GenericFunctions.switchWindow('Customer Service Portal')
+    // it('Select randomly from the list a location from Going to', async () => {
+    //     await GenericFunctions.clickElement(GeneralSelectors.going_to_button)
+    //     await GeneralPage.selectRandomLocationGoingTo()
+    //     await GenericFunctions.clickElement(GeneralSelectors.first_element_going_to_list)
+    // })
+    // it('Select random dates from DatePicker and Apply button', async () => {
+    //     await GenericFunctions.clickElement(GeneralSelectors.dates_button)
+    //     await GeneralPage.selectRandomDatesFromDatePicker()
+    //     await GenericFunctions.clickElement(GeneralSelectors.apply_button)
+    // })
+    // it('Select random Travellers', async () => {
+    //     await GenericFunctions.clickElement(GeneralSelectors.travellers_button)
+    //     await GeneralPage.addRandomTravellers()
+    // })
+    // it('Click the done button', async () => {
+    //     await GenericFunctions.clickElement(GeneralSelectors.done_button)
+    //     await GenericFunctions.validateAttributeElement(GeneralSelectors.travellers_container, "aria-hidden", "true")
+    // })
+    // it('Click the search button ', async () => {
+    //     await GenericFunctions.waitForElementToBeExpectedState(GeneralSelectors.search_button, 'be clickable')
+    //     await GenericFunctions.clickElement(GeneralSelectors.search_button)
+    // })
+    // it('Click What we are paid impacts our sort order link', async () => {
+    //     await GenericFunctions.waitForElementToBeExpectedState(GeneralSelectors.impacts_sort_order_link, 'be clickable')
+    //     await GenericFunctions.clickElement(GeneralSelectors.impacts_sort_order_link)
+    //     await browser.pause(3000)
+    //     await GenericFunctions.switchWindow('Customer Service Portal')
 
-    })
-    it('Validate that the first article toggle is not expanded', async () => {
-        await GenericFunctions.waitForElementToBeExpectedState(CustomerServiceSelectors.first_article_expanded, 'be displayed')
-        await GenericFunctions.clickElement(CustomerServiceSelectors.first_article_toggle)
-        await GenericFunctions.waitForElementToBeExpectedState(CustomerServiceSelectors.first_article_expanded, 'not be present')
-    })
+    // })
+    // it('Validate that the first article toggle is not expanded', async () => {
+    //     await GenericFunctions.waitForElementToBeExpectedState(CustomerServiceSelectors.first_article_expanded, 'be displayed')
+    //     await GenericFunctions.clickElement(CustomerServiceSelectors.first_article_toggle)
+    //     await GenericFunctions.waitForElementToBeExpectedState(CustomerServiceSelectors.first_article_expanded, 'not be present')
+    // })
 })
 
 
